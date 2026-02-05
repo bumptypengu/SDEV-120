ceb9d5ac362f2b0d02e964e11c0fd5e45edc5f4d
bumptypengu <nwoolley@ivytech.edu>
2026-02-05 09:29:19 -0500

    Add files via upload

---
+++ Module 4 - Making Decisions in Python.py
@@ -0,0 +1,15 @@
+for _ in range(2):
+    # saves Student's name as a variable
+    name = input("Please enter your name: ")
+    
+    # defines student's grade as integer, saves as grade variable
+    grade = int(input("Please enter your final grade: "))
+    
+    if grade >= 60:
+        print(f"Hello {name}, you passed this class")
+    else:
+        print(f"Hello {name}, you did not pass this class")
+
+
+    
+    
