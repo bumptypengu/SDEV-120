cdda395ec90f5c08eb9c22753f125ad9f9875f17
bumptypengu <nwoolley@ivytech.edu>
2026-03-01 04:43:09 -0500

    Add files via upload

---
+++ Module 7 - Functions in Python - Nicolas Woolley.py
@@ -0,0 +1,9 @@
+def greater_than(x, y):
+    if (x > y):
+        return True
+    else:
+        return False
+a = 10
+b = 6
+result = greater_than(a,b)   
+print("The statement", a, "is greater than", b, "is", str(result).lower())   
